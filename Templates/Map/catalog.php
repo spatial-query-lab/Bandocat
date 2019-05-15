4388b828b089f577adba44f40eb8b50fdbf5cf4b
Ruben3rd <rortiz18@islander.tamucc.edu>
2019-05-15 08:20:59 -0500

    Merge branch 'feature/job-folder-cataloging' into bugfix/error-handling
    # Conflicts:
    #	Templates/Folder/catalog.php

--- Templates/Map/catalog.php
+++ Templates/Map/catalog.php
@@ -644,5 +644,26 @@ $readrec = array("POOR","GOOD","EXCELLENT");
 
 </script>
 
+<script>
+
+    // Shows needs review block to admins only
+    function showNeedsReview(){
+        var userRole = "<?php echo $userRole ?>";
+        if ((userRole === "Admin") || (userRole === "admin") === true){
+            //document.getElementById('needsReview').style.display = 'none';
+            console.log('Display. User is admin');
+        }
+        else{
+            document.getElementById('needsReview').style.display = 'none';
+            console.log("Hide. User is not admin");
+        }
+    }
+
+    function resetPage(){
+        window.location.reload();
+    }
+
+</script>
+
 </body>
 </html>
